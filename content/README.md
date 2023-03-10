f0b15d8fb2fd86ee563939559b38752dcc47696a
ssibongee <ssibongee@toss.im>
2023-03-11 03:11:23 +0900

    Initialize Digital Garden 🪴

---
+++ content/README.md
@@ -0,0 +1,19 @@
+---
+date: 2023-03-11
+updated: 2023-03-11
+tags:
+  - README
+---
+
+# Digital Garden 🪴
+
+Digital Garden for Volatile Memory
+
+### Usage
+- [[Roam Research]] 스타일의 문서 링크를 지원하는 도구를 사용해요.
+	- [Roam Research](https://roamresearch.com/)
+	- [Obsidian](https://obsidian.md/)
+	- [Foam](https://foambubble.github.io/)
+- `content/` 경로에 컨텐츠를 작성해 주세요.
+	- 서브디렉토리나 다른 디렉토리는 없어도 괜찮아요!
+	- 하이퍼텍스트와 연결을 중심으로 작성해 보세요.
---
+++ gatsby-config.js
@@ -0,0 +1,62 @@
+const path = require("path");
+const pathPrefix = "/";
+const siteMetadata = {
+  title: "Digital Garden",
+  shortName: "Digital Garden",
+  description: "Digital Garden for Volatile Memory",
+  siteUrl: "https://ssibongee.github.io/digital-garden",
+};
+
+module.exports = {
+  siteMetadata,
+  pathPrefix,
+  flags: {
+    DEV_SSR: true,
+  },
+  plugins: [
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "content",
+        path: `./content`,
+      },
+    },
+    {
+      resolve: "gatsby-theme-primer-wiki",
+      options: {
+        nav: [
+
+          {
+            title: "Github",
+            url: "https://github.com/ssibongee",
+          },
+        ],
+        editUrl: "https://github.com/ssibongee/digital-garden/blob/master/content/",
+        sidebarComponents: ["latest", "tag"],
+        lastUpdatedText: "최근 업데이트",
+        shouldSupportLatest: true,
+      },
+    },
+    {
+      resolve: "gatsby-plugin-manifest",
+      options: {
+        name: siteMetadata.title,
+        short_name: siteMetadata.shortName,
+        start_url: pathPrefix,
+        background_color: `#2596be`,
+        display: `standalone`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-sitemap`,
+    },
+    {
+      resolve: "gatsby-plugin-robots-txt",
+      options: {
+        host: siteMetadata.siteUrl,
+        sitemap: `${siteMetadata.siteUrl}/sitemap/sitemap-index.xml`,
+        policy: [{ userAgent: "*", allow: "/" }],
+      },
+    },
+  ],
+};
